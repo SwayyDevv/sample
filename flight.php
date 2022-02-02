4c280353d65cade3afaf9b28a65913c3d20c6080
SwayyDev <98856254+SwayyDevv@users.noreply.github.com>
2022-02-01 19:04:02 -0800

    Add files via upload

---
+++ flight.php
@@ -0,0 +1,359 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Homepage</title>
+	<title>Contact Me</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="flight.css">
+	<link rel="stylesheet" type="text/css" href="fontawesome/css/all.css">
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrapcss.css">
+    <script type="text/javascript" src="JQuery-3.6.0.min.js"></script>
+   <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Fredoka+One&family=Open+Sans:wght@400;500;600;700;800&family=Poppins:wght@600;700;800&family=Raleway:wght@600;700;800;900&family=Rubik:ital,wght@0,500;0,600;0,700;1,300&display=swap" rel="stylesheet">
+</head>
+<body>
+  <header>
+    <div class="wrapper">
+      <div class="logo">
+      <a href="#" >Fly fox</a>
+    </div>
+      
+    
+
+    <div class="a-btn">
+    <a href="#">Destination</a>
+    <a href="#">Hotel </a>
+    <a href="#">Flights</a>
+    <a href="#">Bookings</a>
+    <a href="#">Login</a>
+    <a href="#" >sign up</a>
+    </div>
+
+    <!-- <div class="icon">
+    <a href="#" onclick="return togglenavbar()">
+    <i class="fa fa-align-center"></i></a>
+    <div> -->
+      
+    </div>
+  
+    
+  </header>
+
+    <div class="container-fluid">
+    	<div class="wrappe">
+       <div class="row" >
+        <div class="col-md-7">
+          <div class="travel">
+            <h1 class="title" id="glow">Best destination around the world</h1>
+          <div class="side-text">Travel,enjoy and live a new and full life.<i class="fa fa-stroopwafel fa-spin fa-3x fa-fw" style="font-size: 60px;"></i><span class="sr-only">Loading...</span></div>
+          <div class="text"><p>Find the best deal and travel across the globe easily with fox flight.<b><br>LOWEST PRICE GUARANTEE!!!</b></p></div>
+
+      <div>
+          <button class="reg-button">Find out more</button>
+         <button class="demo-button"><span class="fas fa-play"></span>&nbsp; play Demo</button>
+       </div>
+
+        </div>
+    </div>
+
+        <div class="col-md-5"> 
+          <div class="pic-Box">
+             <div class="PB">
+                <img src="pictures/heli.png" style="width: 270px;margin-left: -155px;position: relative;">
+  
+             </div>
+             <span><h2></h2></span>
+             <span style="float: right;"><h2></h2></span>
+            <img src="pictures/smiling.png" style="width: 530px; margin-top: -100px;">
+            <span><h2></h2></span>
+          </div>
+        </div>
+
+          </div> 
+      </div>
+     </div>
+    <div class="container mt-5 mb-5">
+           <div class="wrap">
+             <div class="col-md-12 text-center">
+            <h5 class="services">we offer the best services</h5>   
+           </div>
+           </div>
+    </div>
+   
+
+    <div class="container-fluid mt-5 mb-5">
+      <div class="wra">
+         <div class="row">
+            <div class="col-md-3 text-center">
+                <div class="img-container" >
+                 <img src="pictures/sat.jpg">
+                <h5>Calculated weather</h5>
+                <p>For the saftery of our customers we built widget longer admire<br>
+                do the barton vanity itslef will do it.</p>
+                    </div>            
+                </div>
+
+
+
+            <div class="col-md-3 text-center ">
+                <div class="img-container">
+                   <img src="pictures/plane.jpg" style="width: 70%;">
+                <h5 style="margin-top:50px; ">Best Flights</h5>
+                <p>We offer best flights built widget longer admire<br>
+                do the barton vanity itslef will do it.</p>
+ 
+                </div>
+            </div>
+
+
+
+            <div class="col-md-3 text-center ">
+                <div class="img-container">
+                   <img src="pictures/speaker.jpg">
+                <h5>Local Events</h5>
+                <p>Listen to the sound we offers<br>
+                varierty of music festivals just for our customers which is free.</p>
+            </div>
+            </div>
+
+
+             <div class="col-md-3 text-center">
+                <div class="img-container">
+                <img src="pictures/cus.jpg">
+                <h5>Customization</h5>
+                <p>We deliver customized aviation services<br>
+                for military customers. budget prices and affordability.</p>
+            </div>    
+         </div>
+     </div>
+      </div> 
+        
+</div>
+
+
+<div class="container mt-5 mb-5">
+    <div class="row">
+        <div class="col-md-12 text-center">
+              <p class="top-desc">top selling</p>
+            <h3 class="desc">top  destinations</h3>
+        </div>
+    </div>
+</div>
+
+
+<div class="container-fluid">
+  <div class="background">
+    <div class="row">
+      <div class="col-md-4">
+          <div class="picture-container" style=" cursor: pointer;">
+            <img src="pictures/dubai.webp">
+            <div class="cities">
+              <p>Dubai,UAE. &nbsp; <span>$15k</span></p>
+              <i class="fas fa-location-arrow">20 days trip</i>
+
+            </div>
+          </div>
+      </div>
+
+      <div class="col-md-4">
+          <div class="picture-container" >
+            <img src="pictures/paris.jpg">
+            <div class="cities">
+              <p>Paris,France. &nbsp;<span>$35k</span></p>
+             <i class="fas fa-location-arrow">40 days trip</i>
+            </div>
+          </div>
+      </div>
+
+
+      <div class="col-md-4">
+          <div class="picture-container">
+            <img src="pictures/newyork.jpg">
+            <div class="cities">
+              <p>Eden Island,Seychelles.&nbsp;<span>$10k</span> </p>
+              <i class="fas fa-location-arrow">10 days trip</i>
+            </div>
+          </div>
+      </div>
+
+  </div>  
+  </div>
+</div>
+
+<div class="container-fluid mt-5">
+<div class="back">
+  <div class="row">
+    <div class="col-md-7 mt-5">
+      <div class="division">
+      <h5>easy and fast</h5>
+      <h1>book your next trip in 3 easy steps</h1>
+      <span class="fas fa-location-arrow"></span><span class="title">&nbsp;Choose Destination</span>
+      <p>Choose a destination most suitable to you and family and love ones amongs our varieties of destinations and locations so you can enjoy your new enviroment with them.</p>  
+      </div>
+       <div class="division">
+        <span class="fas fa-money-bill"></span><span class="title">&nbsp;Make payment</span>
+      <p>Choose a destination most suitable to you and family and love ones amongs our varieties of destinations and locations so you can enjoy your new enviroment with them.</p>
+      </div>
+       <div class="division">
+      <span class="fas fa-car"></span><span class="title">&nbsp;Reach airport on selected dates</span>
+      <p>Choose a destination most suitable to you and family and love ones amongs our varieties of destinations and locations so you can enjoy your new enviroment with them.</p>
+      </div>
+    </div>
+
+  <div class="col-md-5 mt-5">
+    <div class="pic-container">
+      <img src="pictures/gre.webp">
+      <h5 style="font-family: 'Poppins',sans-serif;">Trip to Amalfi Coast,Italy.</h5>
+      <p>24-12 December | By Rookie</p>
+      <span class="fas fa-location-arrow" style="margin-right: 10px;"></span><span class="fas fa-bookmark" style="margin-right: 10px;"></span><span class="fas fa-map-marker-alt"></span>
+      <p>57 people going &nbsp;<i class="far fa-heart"></i></p>
+    </div>
+  </div>
+
+  </div>
+</div>
+  
+</div>
+
+<div class="gry-background mt-5">
+  <div class="divv">
+    <div class="container-fluid">
+  <div class="row">
+    <div class="col-md-7">
+       <div class="gry-text">
+        <h3>Testimonial</h3>
+        <span>What people say about us</span>
+      <div class="gry-icons"><i class="fas fa-circle">&nbsp;&nbsp;</i><i class="far fa-circle">&nbsp;&nbsp;</i><i class="far fa-circle">&nbsp;&nbsp;</i></div>
+    </div>
+    </div>
+      
+
+      
+    
+
+    <div class="col-md-5 mt-5">
+      <div class="people-talk">
+        <div class="paragraph">
+  
+          <img src="pictures/girl.jpg">
+        <p>Had one of the best expriences using fox flight i was really comfortable on the flight and i landed in my destination safely. i really fox flight becauase they are really good at attending to their customers.The local event was fun too i really had fun thank you fox flight </p>
+        <h5>Rookie Swavey</h5>
+        <h4>Lagos,Nigeria</h4>
+        </div>
+        <div class="gry-2">
+        <p>Had one of the best expriences using fox flight i was really comfortable on the flight and i landed in my destination safely. i really fox flight becauase they are really good at attending to their customers.The local event was fun too i really had fun thank you fox flight </p>
+        <h5>Oshia Gift</h5>
+        <h4>Lagos,Nigeria</h4>
+        </div>
+
+      </div>
+    </div>
+  </div>
+  </div>
+
+  <div class="container-fluid mt-5">
+    <div class="partners">
+      <div class="row">
+        <div class="col-md-3">
+          <img src="pictures/google.jpg">
+        </div>
+        <div class="col-md-3">
+          <img src="pictures/paypal.png" style="margin-top: 24px;">
+        </div>
+        <div class="col-md-3">
+          <img src="pictures/payment.png">
+        </div>
+        <div class="col-md-3">
+          <img src="pictures/blockchain.jpg">
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container-fluid">
+    <div class="subscribe">
+      <div class="row">
+        <div class="col-md-12">
+          <h4>Subscribe to our newletters to get information and other interesting offer<br>from Fox Flight</h4>
+          <form class="actions">
+            <i class="fas fa-envelope"></i>
+            <input type="text" name="" placeholder="Enter email">
+            <button>Subscribe</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container-fluid">
+    <div class="fox">
+      <div class="row">
+        <div class="col-md-3">
+          <h5>Fox Flight</h5>
+          <p> ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis.</p>
+        </div>
+       <div class="col-md-6">
+         <div class="row">
+          <div class="col-md-4"> 
+            <ul>
+              <h5>Company</h5>
+              <li><a href="#">Expedia</a></li>
+              <li><a href="#">Qantas</a></li>
+              <li><a href="#">Axon</a></li>
+            </ul>
+          </div>
+         <div class="col-md-4">
+           
+            <ul>
+              <h5>destination</h5>
+              <li><a href="#">Expedia</a></li>
+              <li><a href="#">Qantas</a></li>
+              <li><a href="#">Axon</a></li>
+         </div>
+         <div class="col-md-4">
+           
+           <ul>
+            <h5>More</h5>
+               <li><a href="#">Expedia</a></li>
+              <li><a href="#">Qantas</a></li>
+              <li><a href="#">Axon</a></li>
+           </ul>
+         </div> 
+         </div>      
+       </div>
+        <div class="col-md-3" style="margin-top: 48px;">
+          <h3>Download our app</h3>
+            <i class="fab fa-google-play"></i><button>&nbsp;Playstore</button>
+            <i class="fab fa-app-store"></i><button>&nbsp;Appstore</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+
+<script type="text/javascript">
+
+    function togglenavbar(){
+      var navigationBar= document.getElementById('navigationBar');
+      if(navigationBar.className== "navigationBar"){
+        navigationBar.className+= " responsive";
+        return false;
+            }
+            else{
+              navigationBar.className= "navigationBar";
+            }
+    }
+    </script>
+
+
+
+
+
+</body>
+</html>
